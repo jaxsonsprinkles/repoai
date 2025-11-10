1c9a0114a3cf65994f9a9826b348e76552b1ac3d
terraboi4 <jaxstar24@gmail.com>
2025-11-10 11:35:25 -0600

    add ai summary

--- app/page.jsx
+++ app/page.jsx
@@ -1,73 +1,144 @@
-'use client';
+"use client";
 
-import fetchData from '@/lib/fetchData';
-import { File, Folder } from 'lucide-react';
-import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import fetchContents from "@/lib/fetchContent";
+import fetchTree from "@/lib/fetchTree";
+import getAI from "@/lib/getAI";
+import { File, Folder } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
-	const [tree, setTree] = useState(null);
-	const { register, handleSubmit } = useForm();
+  const [tree, setTree] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [summary, setSummary] = useState(null);
+  const { register, handleSubmit } = useForm();
 
-	const onSubmit = async (data) => {
-		const result = await fetchData(data.link, data.branch);
-		const order = ['tree', 'blob'];
-		result.tree.sort((a, b) => {
-			return order.indexOf(a.type) - order.indexOf(b.type);
-		});
-		setTree(result.tree);
-		console.log(result.tree);
-	};
+  const onSubmit = async ({ link, branch }) => {
+    const result = await fetchTree(link, branch);
+    const order = ["tree", "blob"];
+    result.tree.sort((a, b) => {
+      return order.indexOf(a.type) - order.indexOf(b.type);
+    });
 
-	return (
-		<div className='p-3 space-y-3'>
-			<h1 className='text-4xl font-bold text-center'>RepoAI</h1>
-			<form
-				className='space-x-2 flex justify-center'
-				onSubmit={handleSubmit(onSubmit)}
-			>
-				<div className='flex gap-2'>
-					<input
-						{...register('link')}
-						type='text'
-						className='input input-primary w-3/4'
-						placeholder='Enter Github link here...'
-					/>
-					<input
-						{...register('branch')}
-						type='text'
-						className='input input-primary w-1/4'
-						placeholder='Branch'
-					/>
-				</div>
-				<button className='btn btn-primary' type='submit'>
-					Submit
-				</button>
-			</form>
+    setTree(result);
+  };
 
-			{tree && (
-				<ul className='list bg-base-200 rounded-box shadow-md overflow-y-scroll max-h-1/2 w-7/8 lg:w-3/4 xl:w-1/2 mx-auto'>
-					{tree.map((file, i) => {
-						if (file.path.includes('/')) {
-							return;
-						}
-						return (
-							<li key={i} className='list-row p-3!'>
-								<div>
-									{file.type == 'blob' ? (
-										<File className='w-5 h-5' />
-									) : (
-										<Folder className='w-5 h-5' />
-									)}
-								</div>
-								<div>
-									<div>{file.path}</div>
-								</div>
-							</li>
-						);
-					}) ?? <p>Loading...</p>}
-				</ul>
-			)}
-		</div>
-	);
+  const getSummary = async ({ link, path }) => {
+    document.getElementById("my_modal_3").showModal();
+    setLoading(true);
+    const result = await fetchContents(link, path);
+
+    const response = await getAI(path, result);
+    setSummary(response);
+    console.log(response);
+  };
+
+  return (
+    <div className="p-3 space-y-3">
+      <h1 className="text-4xl font-bold text-center">RepoAI</h1>
+      <form
+        className="space-x-2 flex justify-center"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="flex gap-2">
+          <input
+            {...register("link")}
+            type="text"
+            className="input input-primary w-3/4"
+            placeholder="Enter Github link here..."
+          />
+          <input
+            {...register("branch")}
+            type="text"
+            className="input input-primary w-1/4"
+            placeholder="Branch"
+          />
+        </div>
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+      </form>
+
+      {tree && (
+        <div className="outer-container w-7/8 lg:w-3/4 xl:w-1/2 mx-auto max-h-1/2">
+          <ul className="list bg-base-200 rounded-box shadow-md p-4 inner-scroll-wrapper">
+            {tree.tree.map((file, i) => {
+              if (file.path.includes("/")) {
+                return;
+              }
+              return (
+                <div
+                  key={i}
+                  className="tooltip"
+                  data-tip={`Summarize ${file.path} with AI`}
+                >
+                  <dialog id="my_modal_3" className="modal">
+                    <div className="modal-box">
+                      <form method="dialog">
+                        <button
+                          onClick={() => {
+                            setSummary(null);
+                            setLoading(false);
+                          }}
+                          className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                        >
+                          ✕
+                        </button>
+                      </form>
+                      <div className="flex flex-col gap-3 p-3">
+                        {!summary ? (
+                          <div>
+                            <h2 className="text-xl font-semibold mb-3">
+                              Your AI summary is loading...
+                            </h2>
+                            <div className="skeleton h-4"></div>
+                            <div className="skeleton h-4"></div>
+                            <div className="skeleton h-4"></div>
+                            <div className="skeleton h-4"></div>
+                            <div className="skeleton h-4"></div>
+                            <div className="skeleton h-4"></div>
+                            <div className="skeleton h-4"></div>
+                            <div className="skeleton h-4"></div>
+                            <div className="skeleton h-4 w-1/2"></div>
+                          </div>
+                        ) : (
+                          <div>
+                            <h2 className="text-xl font-semibold">
+                              AI Summary of Your File
+                            </h2>
+                            <p>{summary}</p>
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  </dialog>
+                  <div
+                    onClick={() => {
+                      getSummary({
+                        link: tree.url,
+                        path: file.path,
+                      });
+                    }}
+                  >
+                    <li className="hover:border-2 hover:border-primary cursor-pointer list-row p-3! transition-transform duration-150 hover:scale-110">
+                      <div>
+                        {file.type == "blob" ? (
+                          <File className="w-5 h-5" />
+                        ) : (
+                          <Folder className="w-5 h-5" />
+                        )}
+                      </div>
+                      <div>
+                        <div>{file.path}</div>
+                      </div>
+                    </li>
+                  </div>
+                </div>
+              );
+            }) ?? <p>Loading...</p>}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
