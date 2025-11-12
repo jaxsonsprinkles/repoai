342e6e881f2b744d2447c8a1507c614514c26462
terraboi4 <jaxstar24@gmail.com>
2025-11-12 12:48:08 -0600

    improve main section

--- components/FileTree.jsx
+++ components/FileTree.jsx
@@ -1,58 +1,55 @@
-import { File, Folder, Star } from 'lucide-react';
+import { File, Folder, Star } from "lucide-react";
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
-} from './ui/table';
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
 
 export default function FileTree({ tree, onFileClick }) {
-	const rootFiles = tree.tree.filter((file) => {
-		return !file.path.includes('/');
-	});
-	TableHead;
-
-	return (
-		<div className='outer-container mx-auto overflow-y-scroll border rounded-lg'>
-			<Table>
-				<TableHeader>
-					<TableRow>
-						<TableHead className='font-bold'>
-							{tree.url.match(/repos\/[^/]+\/([^/]+)\/git\/trees\//)[1]}
-						</TableHead>
-					</TableRow>
-				</TableHeader>
-				<TableBody>
-					{rootFiles.map((file, i) => {
-						return (
-							<FileItem
-								key={i}
-								file={file}
-								onClick={() => {
-									onFileClick(file);
-								}}
-							/>
-						);
-					})}
-				</TableBody>
-			</Table>
-		</div>
-	);
+  return (
+    <div className="outer-container mx-auto overflow-y-scroll border rounded-lg">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="font-bold">
+              {tree.url.match(/repos\/[^/]+\/([^/]+)\/git\/trees\//)[1]}
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {tree.tree.map((file, i) => {
+            return (
+              <FileItem
+                key={i}
+                file={file}
+                onClick={() => {
+                  onFileClick(file);
+                }}
+              />
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
 
 function FileItem({ file, onClick }) {
-	return (
-		<TableRow className='cursor-pointer list-row p-3!' onClick={onClick}>
-			<TableCell>
-				{file.type == 'blob' ? (
-					<File className='size-5' />
-				) : (
-					<Folder className='size-5' />
-				)}
-			</TableCell>
-			<TableCell>{file.path}</TableCell>
-		</TableRow>
-	);
+  const depth = file.path.split("/").length - 1;
+  const indent = depth * 16;
+  return (
+    <TableRow className="cursor-pointer list-row p-3!" onClick={onClick}>
+      <TableCell style={{ paddingLeft: `${indent}px` }}>
+        {file.type == "blob" ? (
+          <File className="size-5" />
+        ) : (
+          <Folder className="size-5" />
+        )}
+      </TableCell>
+      <TableCell>{file.path}</TableCell>
+    </TableRow>
+  );
 }
---
+++ components/ui/code-block.jsx
@@ -0,0 +1,78 @@
+"use client";
+import React from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import duotoneLight from "react-syntax-highlighter/dist/cjs/styles/prism/duotone-light";
+export const CodeBlock = ({
+  language,
+  filename,
+  code,
+  highlightLines = [],
+  tabs = [],
+}) => {
+  const [copied, setCopied] = React.useState(false);
+  const [activeTab, setActiveTab] = React.useState(0);
+
+  const tabsExist = tabs.length > 0;
+
+  const activeCode = tabsExist ? tabs[activeTab].code : code;
+  const activeLanguage = tabsExist
+    ? tabs[activeTab].language || language
+    : language;
+  const activeHighlightLines = tabsExist
+    ? tabs[activeTab].highlightLines || []
+    : highlightLines;
+
+  return (
+    <div className="bg-neutral-100 relative w-full rounded-lg p-4 font-mono text-sm">
+      <div className="flex flex-col gap-2">
+        {tabsExist && (
+          <div className="flex  overflow-x-auto">
+            {tabs.map((tab, index) => (
+              <button
+                key={index}
+                onClick={() => setActiveTab(index)}
+                className={`px-3 !py-2 text-xs transition-colors font-sans ${
+                  activeTab === index
+                    ? "text-white"
+                    : "text-zinc-400 hover:text-zinc-200"
+                }`}
+              >
+                {tab.name}
+              </button>
+            ))}
+          </div>
+        )}
+        {!tabsExist && filename && (
+          <div className="flex justify-between items-center py-2">
+            <div className="text-xs text-zinc-400">{filename}</div>
+          </div>
+        )}
+      </div>
+      <SyntaxHighlighter
+        language={activeLanguage}
+        style={duotoneLight}
+        customStyle={{
+          margin: 0,
+          padding: 0,
+          background: "transparent",
+          fontSize: "0.875rem", // text-sm equivalent
+        }}
+        wrapLines={true}
+        showLineNumbers={true}
+        lineProps={(lineNumber) => ({
+          style: {
+            backgroundColor: activeHighlightLines.includes(lineNumber)
+              ? "rgba(255,255,255,0.1)"
+              : "transparent",
+            display: "block",
+            width: "100%",
+          },
+        })}
+        PreTag="div"
+      >
+        {String(activeCode)}
+      </SyntaxHighlighter>
+    </div>
+  );
+};
